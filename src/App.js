19cce1947936cecd4ad29081dcce61da85d243d0
harpreet1602 <harpreets1602@gmail.com>
2021-08-05 23:24:10 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+// first of all login can be done using google after that we will be redirected to posts page
+// then we will be entering our current blog and enter then it will be stored into the database
+// after that we can go to all blogs page by the button then we will be able to see the 
+// uid matched document's body according to the user logged in 
+
+
 import { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./Login";
