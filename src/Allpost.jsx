d78eac9e007f2be39415145684553c393f81d441
harpreet1602 <harpreets1602@gmail.com>
2021-08-05 22:39:49 +0530

    update

--- src/Allpost.jsx
+++ src/Allpost.jsx
@@ -1,10 +1,37 @@
 
-
+import { useEffect, useState } from "react";
+import { auth, firestore } from "./firebase";
+import "./Allpost.css";
 let Allpost = (props)=>{
+    let [posts,setPosts] = useState([]);
+
+    useEffect(()=>{
+        let f=async ()=>{
+            let querySnapshot = await firestore.collection("posts").get();
+
+            let tempArr = [];
+            querySnapshot.forEach((doc)=>{
+                tempArr.push({
+                    uid: doc.uid,
+                    data:doc.data()
+                })
+            })
+            setPosts(tempArr);
+            console.log(posts);
+        }
+        f();
+    },[]);
 
     return(
         <div>
-            
+            <ul>
+            {
+                posts.map((el)=>{
+                    if(el.data.uid==props.user.uid)
+                    return <li key={el.id}>{el.data.body}</li>;
+                })
+            }
+            </ul>
         </div>
     )
 }
--- src/Post.jsx
+++ src/Post.jsx
@@ -1,20 +1,41 @@
-import {Redirect} from "react-router-dom";
-import { auth } from "./firebase";
+import {Redirect,Link} from "react-router-dom";
+import { auth, firestore } from "./firebase";
+
+
+import "./style.css";
 let Post = (props)=>{
     console.log(props.user?"true":"false");
+   
+
+
     return(
         <div>
             {props.user?(
-                <>
+                <div className="post">
                 <h1>Hello {props.user.displayName}</h1>
                 <p>Email: {props.user.email}</p>
-                
+                <input type="text" 
+                onKeyDown={(e)=>{
+                  if(e.key=="Enter")
+                  {
+                    let post = e.currentTarget.value;
+                    firestore.collection("posts").add({
+                        body:post,
+                        uid:props.user.uid
+                    });
+                    e.currentTarget.value="";
+              }
+                }}
+                className="input"/>
+                <br/>
+                <Link to="/allpost"><button className="btn btn-success"
+                >View All Blogs</button></Link>
                 <button onClick={()=>{
                     auth.signOut();
                 }}
                 className="btn btn-info m-4"
                 >Logout</button>
-                </>
+                </div>
 
              )
             
